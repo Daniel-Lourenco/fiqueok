1870cd9cdbfb47938485bd01ca624998606cb590
Daniel-Lourenco <daniel.lorenco@hotmail.com>
2022-05-29 16:00:58 -0300

    Implementação da mesclagem com o Datepicker da Taninha

--- app/src/main/java/com/example/fiqueok/ui/agendamento/DatePickerFragment.kt
+++ app/src/main/java/com/example/fiqueok/ui/agendamento/DatePickerFragment.kt
@@ -10,10 +10,10 @@ import java.util.*
 class DatePickerFragment(val callback: (result: String) -> Unit) : DialogFragment(), DatePickerDialog.OnDateSetListener{
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val c: Calendar = Calendar.getInstance()
-        val year: Int = c.get(Calendar.YEAR)
-        val month: Int = c.get(Calendar.MONTH)
-        val day: Int = c.get(Calendar.DAY_OF_MONTH)
+        val myCalendar: Calendar = Calendar.getInstance()
+        val year: Int = myCalendar.get(Calendar.YEAR)
+        val month: Int = myCalendar.get(Calendar.MONTH)
+        val day: Int = myCalendar.get(Calendar.DAY_OF_MONTH)
         return DatePickerDialog(requireContext(), this, year, month, day)
     }
 
